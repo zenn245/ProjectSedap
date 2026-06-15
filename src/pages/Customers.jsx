b395543c16b2c7577b8bd1e2a1e55869a0669b17
zenn2143 <znuchrw@gmail.com>
2026-06-15 08:18:21 +0700

    p13

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,10 +1,45 @@
 import PageHeader from "../components/PageHeader";
 
-export default function Customers(){
-    return(
-        <div>
-            <PageHeader/>
-            ini halaman customers
+export default function Customers() {
+  return (
+    <div>
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-    )
+      </dialog>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <PageHeader />
+      ini halaman customers
+    </div>
+  );
 }
